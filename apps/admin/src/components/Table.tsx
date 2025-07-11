f5f5b4836a5fcece3a105ee7f54c7841aa3cb4a1
seevsk <sebastiansalasrojas7@gmail.com>
2025-07-11 18:16:46 -0500

    feat(admin): Daysi and components

---
+++ apps/admin/src/components/Table.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export const Table = () => {
+  return (
+    <div className="overflow-x-auto rounded-box border border-base-content/5 bg-base-100">
+      <table className="table">
+        {/* head */}
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Stock</th>
+            <th>Acciones</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </div>
+  )
+}
