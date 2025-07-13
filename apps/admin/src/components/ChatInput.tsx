a0b9499152c822d755f5fdeea0eff4a1fc3981c0
Daniel Cantu <danielcantu258@gmail.com>
2025-07-13 00:46:06 -0500

    feat(admin):servicesProducts and ChatMassage Html

---
+++ apps/admin/src/components/ChatInput.tsx
@@ -0,0 +1,8 @@
+export function ChatInput() {
+  return (
+   <form><input type="text" placeholder="Type here" className="input" />
+   <button type="submit" className="btn">Default</button>
+   </form>
+   
+  );
+}
