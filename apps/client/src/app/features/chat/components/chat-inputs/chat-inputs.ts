296977e9bbc1db35cf3b28b85cec5a1d0fc79284
Jorge Quevedo <jorgequeve.job@gmail.com>
2025-07-15 21:01:28 -0500

    feat(client):chat-sockets

--- apps/client/src/app/features/chat/components/chat-inputs/chat-inputs.ts
+++ apps/client/src/app/features/chat/components/chat-inputs/chat-inputs.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ChatStateService } from '../../../../service/chat-state-service/chat-state-service';
 import { FormsModule } from '@angular/forms';
+import { ServiceChat } from '../../../../service/chat-service/service-chat';
 
 @Component({
   selector: 'app-chat-inputs',
@@ -11,10 +12,11 @@ import { FormsModule } from '@angular/forms';
 export class ChatInputs {
   message: string = '';
 
-  constructor(private chatStateService: ChatStateService) {}
+  constructor(private chatStateService: ChatStateService, private serviceChat: ServiceChat) {}
 
   send() {
     if (this.message.trim()) {
+      this.serviceChat.sendMessage(this.message)
       this.chatStateService.addMessage({ text: this.message, from: 'user' });
       this.message = '';
     }
